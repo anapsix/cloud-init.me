e0bef5b04176d92955fdc220d6aabb5922fc47b3
Anastas Dancha <anapsix@random.io>
2015-04-16 17:23:12 -0400

    BUNDLER: use bundler, sample config

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source "https://rubygems.org"
 
 gem "rack"
-gem "rack-protection"
-gem "cuba"
-gem "tilt"
+gem "rack-protection", :require => "rack/protection"
+gem "cuba", :require => ["cuba","cuba/render"]
+gem "tilt", :require => "tilt/erb"
+gem "useragent"
+
+gem "rerun", :group => :development
--- cloud-init.rb
+++ cloud-init.rb
@@ -1,12 +1,9 @@
 # userdata.rb
 
-require "cuba"
-require "cuba/render"
-require "erb"
-require "rack/protection"
-require 'securerandom'
-require 'yaml'
+Bundler.require(:default, :development)
 
+require "yaml"
+require "securerandom"
 
 Cuba.use Rack::Session::Cookie, :secret => SecureRandom.base64(42)
 Cuba.use Rack::Protection
